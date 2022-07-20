8642698648dc7639e10aee6883067a83ceb6e206
jkka777 <abhiram.kishore@gmail.com>
2022-07-20 17:36:23 +0530

    fw19_0718_day-2

---
+++ fw19_0718/Support Section/main page/components/navbar.js
@@ -0,0 +1,20 @@
+
+let navbar = () => {
+    return `<div class="manual_logo">
+    <img src="https://theme.zdassets.com/theme_assets/197219/111fe6921e8b22116b39eaa745ea08e38a718e0d.png"
+        alt="User-Manual logo">
+</div>
+<div class="request">
+    <div>
+        <a href="#">Submit a request</a>
+    </div>
+
+</div>`
+}
+
+let footer = () => {
+    return `<div><a href="">Cronometer</a></div>
+    <div><button class="support">Support</button></div>`
+}
+
+export {navbar,footer};
---
+++ fw19_0718/Support Section/main page/scripts/mainpage.js
@@ -0,0 +1,29 @@
+
+// import navbar from "../components/navbar.js";
+
+import { footer, navbar } from "../components/navbar.js";
+
+document.querySelector('.navbar').innerHTML = navbar();
+
+document.querySelector('.footer').innerHTML = footer();
+
+let toWebSupportPage = () => {
+
+    window.location.href = "../sub-pages/web_version/web_version.html";
+    
+}
+
+let toMobSuppPage = () => {
+    window.location.href = "../sub-pages/mobile_version/mob_ver.html";
+}
+
+let toProPage = () => {
+    window.location.href = "../sub-pages/pro_version/pro_ver.html";
+}
+
+
+document.querySelector('.first_box>div:nth-child(1)').addEventListener('click', toWebSupportPage);
+
+document.querySelector('.first_box>div:nth-child(2)').addEventListener('click', toMobSuppPage);
+
+document.querySelector('.first_box>div:nth-child(3)').addEventListener('click', toProPage);
