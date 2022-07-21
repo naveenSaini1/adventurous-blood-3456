35fc468c69f77bc9cf3191e2deb2d46bafed1f4b
dyshendre <103635214+dyshendre@users.noreply.github.com>
2022-07-22 02:09:02 +0530

    fw19_1243

---
+++ fw19_1243/components/navbar.js
@@ -0,0 +1,17 @@
+let navbar=()=>{
+    return `<div>
+    <img src="https://us.v-cdn.net/6030245/uploads/666babe018022fdd94bf429140228002.png" alt="">
+    </div>
+    <div><button>SIGN IN</button>
+        <button>REGISTER</button></div>`;
+}
+
+let mid=()=>{
+    return `<div id="mid">
+<h1>How can we help you?</h1>
+<input type="text" placeholder="Search">
+<button>🔍</button>
+</div>`
+}
+
+export {navbar, mid};
