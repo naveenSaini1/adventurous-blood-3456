02db58c22ed92bb422561227723a2e1b9f7f359f
jkka777 <abhiram.kishore@gmail.com>
2022-07-23 16:08:13 +0530

    added dashboard plans page

---
+++ fw19_0718/dashboard_plans/scripts/dashboard_plans.js
@@ -0,0 +1,6 @@
+
+import { navbar,footer } from "../components/navbar_footer.js";
+
+document.querySelector('.navbar').innerHTML = navbar();
+
+document.querySelector('.footer').innerHTML = footer();
